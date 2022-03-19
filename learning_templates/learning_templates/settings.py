31bd1d7e5672ecb06c1e0756963d838511bedcf7
Aravind <59133679+Aravind-Nani@users.noreply.github.com>
2022-03-19 15:27:48 +0530

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-32&3p@#-_tqdlo^mb(5fswz4u+tk#96dw+787rtdkcch(_(8p1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aravindnani.pythonanywhere.com']
 
 
 # Application definition
